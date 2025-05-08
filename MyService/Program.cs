165aa1fc917b836bd7931a4cde4fa367e5474089
Fernando Rocha <fernando@diagrid.io>
2025-05-07 20:20:14 -0700

    including bug fix for otel

--- MyService/Program.cs
+++ MyService/Program.cs
@@ -1,10 +1,25 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddDapr(); 
-builder.Services.AddDaprClient();            
+builder.Services.AddDaprClient();   
+
+//Bypass bug with OpenTelemetry and Dapr
+// builder.Services.AddOpenTelemetry()
+//     .WithTracing(b =>
+//     {
+//         b.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("myservice"))
+//          .AddAspNetCoreInstrumentation()
+//          .AddHttpClientInstrumentation() 
+//          .AddOtlpExporter(o =>
+//          {
+//              o.Endpoint = new System.Uri("http://localhost:4317");
+//          });
+//     });
 
 var app = builder.Build();
 
